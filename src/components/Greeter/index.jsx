616d53bbb99a0c801a77d7c24f7e08139022db50
Mpinyaz <ebmpinyuri@gmail.com>
2024-03-22 05:47:32 +0200

    Updated Greeter component styles and content for better readability and engagement.

--- src/components/Greeter/index.jsx
+++ src/components/Greeter/index.jsx
@@ -10,6 +10,7 @@ export function Greeter() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     const greeting = new SplitType("#greeting");
+    greeting.chars;
     const greetingChar = document.querySelectorAll("#greeting .char");
     for (let i = 0; i < greetingChar.length; i++) {
       greetingChar[i].style.transform = "translateY(-200%)";
@@ -22,8 +23,8 @@ export function Greeter() {
     });
     const splitTypes = document.querySelectorAll(".reveal-type");
     splitTypes.forEach((char, i) => {
-      const text = new SplitType(char, { types: "chars" });
-      gsap.from(text.chars, {
+      const text = new SplitType(char, { types: "words" });
+      gsap.from(text.words, {
         scrollTrigger: {
           trigger: char,
           start: "top 80%",
@@ -33,8 +34,8 @@ export function Greeter() {
         },
         opacity: 0.2,
         stagger: 0.1,
-        duration: 0.001,
-        delay: i * 0.1,
+        duration: 5,
+        delay: 1 * i,
       });
     });
   }, []);
@@ -51,22 +52,19 @@ export function Greeter() {
         </div>
         <div className="basis-3/4 text-white">
           <ReactLenis ref={textRef} autoRaf={false}>
-            <h1 id="greeting" className="text-3xl">
-              Hello There! ;)
+            <h1 id="greeting" className="text-5xl font-bold mb-2">
+              {"Hello There!, I'm Elton"}
             </h1>
-            <p className="reveal-type">
-              Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-              reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-              ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-              Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet
-              voluptate voluptate dolor minim nulla est proident. Nostrud
-              officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
-              occaecat reprehenderit commodo officia dolor Lorem duis laboris
-              cupidatat officia voluptate. Culpa proident adipisicing id nulla
-              nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
-              reprehenderit commodo ex non excepteur duis sunt velit enim.
-              Voluptate laboris sint cupidatat ullamco ut ea consectetur et est
-              culpa et culpa duis.
+            <p className="reveal-type p-0">
+              A versatile professional, adept at both Software Development and
+              Financial Analysis. Specializing as a Full Stack Developer, I
+              excel in crafting dynamic websites and hybrid mobile applications,
+              proficient in JavaScript, TypeScript, Angular, React, and C#.
+              Simultaneously, my background as a Financial Analyst brings
+              expertise in financial modeling, budgeting, and forecasting,
+              contributing to strategic decision-making with insightful analysis
+              and reporting. Eager to tackle new challenges and committed to
+              expanding my skill set across both domains.
             </p>
           </ReactLenis>
         </div>
