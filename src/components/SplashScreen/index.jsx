a85539cb9b333e68d71dba1db3e66d710927c233
Mpinyaz <ebmpinyuri@gmail.com>
2024-04-29 23:30:31 +0200

    Add Splitting library for enhanced text animations. Improve user engagement & visual appeal on Splash Screen.

--- src/components/SplashScreen/index.jsx
+++ src/components/SplashScreen/index.jsx
@@ -2,31 +2,64 @@ import { useLayoutEffect, useRef } from "react";
 import logo from "../../images/dragon.png";
 import gsap from "gsap";
 import React from "react";
+import Splitting from "splitting";
+import "./Splashscreen.css";
 
 export function SplashScreen() {
   const comp = useRef(null);
   const dragonLogo = useRef(null);
+  const splash = document.querySelector(".splash"),
+    splashTitles = document.querySelectorAll(".splash_row_text > h1"),
+    splashSubTitles = document.querySelectorAll(".splash_row_text > h4"),
+    splashSeparator = document.querySelectorAll(".splash_row_separator"),
+    splashMedia = document.querySelector(".splash_media");
+  const splitAnimation = () => {
+    Splitting({
+      target: splashMedia,
+      by: "cells",
+      image: true,
+      rows: 4,
+      col: 1,
+    });
+  };
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      const t1 = gsap.timeline();
-      t1.from("#intro-slider", {
+      const t1 = gsap.timeline({ defaults: { ease: "expo.out" } });
+      gsap.set(splashTitles, { y: "101%" });
+      gsap.set(splashSubTitles, { autoAlpha: 0 });
+      gsap.set(splashSeparator, { width: 0 });
+      splitAnimation();
+      t1.from(splash, {
         xPercent: "-100",
         duration: 1.3,
         delay: 0.3,
       })
-        .from(["#title-1", "#title-2", "#title-3"], {
-          opacity: 0,
-          y: "+=30",
-          stagger: 0.5,
+        .to(splashTitles, {
+          duration: 1.75,
+          y: 0,
+          stagger: 0.055,
         })
-        .to(["#title-1", "#title-2", "#title-3"], {
-          opacity: 0,
-          y: "-=30",
-          delay: 0.3,
-          stagger: 0.5,
+        .to(splashSubTitles, {
+          duration: 1,
+          autoAlpha: 1,
+          ease: "expo.in",
+          stagger: 0.055,
         })
-        .to("#intro-slider", {
-          xPercent: "-100",
+        .to(splashSeparator, { duration: 1.75, width: "100%", stagger: 0.095 })
+        .fromTo(
+          ".cell",
+          { height: "100%", scale: 0.5 },
+          {
+            duration: 1.25,
+            height: "100%",
+            scale: 1,
+            stagger: 0.025,
+            ease: "expo.inOut",
+          },
+          0.5,
+        )
+        .to(splash, {
+          yPercent: "-100",
           duration: 1.3,
         })
         .from(dragonLogo.current, { opacity: 0, duration: 0.5 })
@@ -42,34 +75,65 @@ export function SplashScreen() {
 
   return (
     <div className="relative" ref={comp}>
-      <div
-        id="intro-slider"
-        className="h-screen p-10 bg-teal-700 absolute font-spaceGrotesk text-center font-extrabold top-0 left-0 z-10 w-full flex flex-col justify-center gap-10 tracking-tight"
-      >
-        <h1 className="text-9xl text-white" id="title-1">
-          Software Engineer
-        </h1>
-        <h1 className="text-9xl text-white" id="title-2">
-          Designer
-        </h1>
-        <h1 className="text-9xl text-white" id="title-3">
-          Freelancer
-        </h1>
-      </div>
-      <div className="h-screen flex flex-col bg-gray-950 justify-center place-items-center">
+      <section className="splash font-spaceGrotesk">
+        <div className="splash_wrapper text-white">
+          <div className="splash_media">
+            <figure className="splash_figure">
+              <img
+                src="/src/assets/cherryblossom.jpg"
+                alt="Splash Image"
+                className="splash_image"
+              />
+            </figure>
+          </div>
+          <div className="absolute z-10 top-0 left-0">
+            <div className="splash_row">
+              <div className="splash_row_text">
+                <h1>Software Developer</h1>
+                <h4>Web Dev</h4>
+              </div>
+              <div className="splash_row_separator"></div>
+            </div>
+            <div className="splash_row">
+              <div className="splash_row_text">
+                <h1>UX/UI Design</h1>
+                <h4>Visuals/Animations</h4>
+              </div>
+              <div className="splash_row_separator"></div>
+            </div>
+            <div className="splash_row">
+              <div className="splash_row_text">
+                <h1>Data Analyst</h1>
+                <h4>Reporting</h4>
+              </div>
+              <div className="splash_row_separator"></div>
+            </div>
+            <div className="splash_row">
+              <div className="splash_row_text">
+                <h1>Freelancer</h1>
+                <h4>Digitizing Ideation</h4>
+              </div>
+              <div className="splash_row_separator"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="absolute -z-40 top-0 left-0 h-screen w-full flex flex-col justify-center items-center overflow-x-hidden bg-teal-600">
         <img
           ref={dragonLogo}
           src={logo}
           alt="logo"
-          className="rounded-full cursor-pointer w-[100px] h-[100px] shadow-md"
+          className="rounded-full cursor-pointer shadow-md"
+          width="100px"
+          height="100px"
         />
         <h1
           id="welcome"
-          className="text-9xl font-bold text-gray-100 font-spaceGrotesk"
+          className="text-9xl font-bold text-white font-spacegrotesk uppercase"
         >
-          Enter the Dragon
+          enter the dragon
         </h1>
-      </div>
+      </section>
     </div>
   );
 }
