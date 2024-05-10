764f57d9af0fc4c876d5f86bb11f8ad1b57713a0
Mpinyaz <ebmpinyuri@gmail.com>
2024-05-10 16:19:10 +0200

    Add new menu links with dynamic content for improved user engagement and navigation. Bugs handling.

--- src/components/HomeMenu/animation.js
+++ src/components/HomeMenu/animation.js
@@ -1,9 +1,112 @@
 import gsap from "gsap";
 
 export const animateMenuOverlay = () => {
-  return gsap.timeline({ paused: true }).to(".menu-overlay", {
-    duration: 1.25,
-    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%,0% 100%)",
-    ease: "power4.inOut",
+  gsap.set(".menu-link-item", { y: -800 });
+  return gsap
+    .timeline({ paused: true })
+    .to(".menu-overlay", {
+      duration: 1.25,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%,0% 100%)",
+      ease: "power4.inOut",
+    })
+    .to(".menu-link-item", {
+      y: 0,
+      duration: 1,
+      stagger: 0.1,
+      ease: "power4.inOut",
+      delay: -0.75,
+    });
+};
+
+export const animateMenuOverlayItems = () => {
+  const imageSources = [
+    "src/assets/homeMenu1.jpg",
+    "src/assets/homeMenu2.jpg",
+    "src/assets/homeMenu3.jpg",
+    "src/assets/homeMenu4.jpg",
+    "src/assets/homeMenu5.jpg",
+  ];
+
+  const menuItems = document.querySelectorAll(".menu-link-item");
+
+  menuItems.forEach((item) => {
+    const copyElements = item.querySelectorAll(
+      ".menu-link-info, .menu-link-label, .menu-link-tag",
+    );
+
+    copyElements.forEach((div) => {
+      const copy = div.querySelector("p");
+      if (copy) {
+        const duplicateCopy = document.createElement("p");
+        duplicateCopy.textContent = copy.textContent;
+        duplicateCopy.style.color = "white";
+        div.appendChild(duplicateCopy);
+      }
+    });
   });
+  document.querySelectorAll(".menu-link-item").forEach((item, index) => {
+    item.addEventListener("mouseover", () => {
+      mouseOverAnimation(item);
+      appendImages(imageSources[index]);
+    });
+
+    item.addEventListener("mouseout", () => {
+      mouseOutAnimation(item);
+    });
+  });
+  const appendImages = (src) => {
+    const preview1 = document.querySelector(".preview-img-1");
+    const preview2 = document.querySelector(".preview-img-2");
+
+    const img1 = createAndAppendImage(src);
+    const img2 = createAndAppendImage(src);
+
+    preview1.appendChild(img1);
+    preview2.appendChild(img2);
+
+    gsap.to([img1, img2], {
+      clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+      duration: 1,
+      ease: "power3.out",
+      onComplete: () => {
+        removeExtraImages(preview1);
+        removeExtraImages(preview2);
+      },
+    });
+  };
+
+  const createAndAppendImage = (src) => {
+    const img = document.createElement("img");
+    img.src = src;
+    img.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)";
+    return img;
+  };
+
+  const removeExtraImages = (cont) => {
+    while (cont.children.length > 10) {
+      cont.removeChild(cont.firstChild);
+    }
+  };
+
+  const mouseOverAnimation = (elem) => {
+    gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
+      top: "-100%",
+      duration: 0.5,
+    });
+    gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
+      top: "0%",
+      duration: 0.5,
+    });
+  };
+
+  const mouseOutAnimation = (elem) => {
+    gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
+      top: "0%",
+      duration: 0.5,
+    });
+    gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
+      top: "100%",
+      duration: 0.5,
+    });
+  };
 };
--- src/components/HomeMenu/index.jsx
+++ src/components/HomeMenu/index.jsx
@@ -4,79 +4,116 @@ import "./HomeMenu.css";
 import { useEffect, useRef, useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { useGSAP } from "@gsap/react";
-import { animateMenuOverlay } from "./animation";
+import { animateMenuOverlay, animateMenuOverlayItems } from "./animation";
 const menuLinks = [
-  { path: "/", label: "Home" },
-  { path: "/about", label: "About" },
-  { path: "/Skills", label: "Skills" },
-  { path: "/experience", label: "Experience" },
-  { path: "/contact", label: "Contact" },
+        { path: "/", label: "Home", info: "Enter the Dragon", tag: "Index" },
+        { path: "/about", label: "Profile", info: "Find out More", tag: "About" },
+        {
+                path: "/Skills",
+                label: "Skills",
+                info: "Core Technologies",
+                tag: "ToolBox",
+        },
+        {
+                path: "/experience",
+                label: "Experience",
+                info: "Time Under the Clock",
+                tag: "Career",
+        },
+        { path: "/contact", label: "Contact", info: "Let's Talk", tag: "Connect" },
 ];
 function HomeMenu() {
-  const container = useRef(null);
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const tl = useRef();
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
-  useGSAP(
-    () => {
-      tl.current = animateMenuOverlay();
-    },
-    { scope: container },
-  );
+        const container = useRef(null);
+        const [isMenuOpen, setMenuOpen] = useState(false);
+        const tl = useRef();
+        const toggleMenu = () => {
+                setMenuOpen(!isMenuOpen);
+        };
+        const navigate = (linkPath) => {
+                toggleMenu();
+                window.location.href = linkPath;
+        };
+        useGSAP(
+                () => {
+                        tl.current = animateMenuOverlay();
+                        animateMenuOverlayItems();
+                },
+                { scope: container },
+        );
 
-  useEffect(() => {
-    if (isMenuOpen) {
-      tl.current.play();
-    } else {
-      tl.current.reverse();
-    }
-  }, [isMenuOpen]);
-  return (
-    <div className="home-menu-container" ref={container}>
-      <div className="menu-bar">
-        <div className="menu-logo">
-          <Link to="/" className="logo text-white font-extrabold text-3xl">
-            Code Dragon
-          </Link>
-        </div>
-        <div className="menu-open relative" onClick={toggleMenu}>
-          <div className="absolute left-0 pulsating-circle -z-10"></div>
-          <p className="text-white font-extrabold uppercase text-3xl">Menu</p>
-        </div>
-      </div>
-      <div className="menu-overlay">
-        <div className="menu-overlay-bar">
-          <div className="menu-logo">
-            <Link to="/" className="logo">
-              Code Dragon
-            </Link>
-          </div>
-          <div className="menu-close">
-            <p>Close</p>
-          </div>
-        </div>
-        <div className="menu-close-icon" onClick={toggleMenu}>
-          <RiCloseCircleLine size={50} />
-        </div>
-        <div className="menu-copy">
-          <div className="menu-links">
-            {menuLinks.map((link, index) => (
-              <div className="menu-link-item" key={index}>
-                <div className="menu-link-item-holder" onClick={toggleMenu}>
-                  <Link to={link.path} className="menu-link text-white">
-                    {link.label}
-                  </Link>
+        useEffect(() => {
+                if (isMenuOpen) {
+                        tl.current.play();
+                } else {
+                        tl.current.reverse();
+                }
+        }, [isMenuOpen]);
+
+        return (
+                <div className="home-menu-container" ref={container}>
+                        <div className="menu-bar">
+                                <div
+                                        className="menu-logo"
+                                        onClick={() => {
+                                                navigate("/");
+                                        }}
+                                >
+                                        <div className="logo text-white font-extrabold">Code Dragon</div>
+                                </div>
+                                <div className="menu-open relative" onClick={toggleMenu}>
+                                        <div className="absolute left-0 pulsating-circle -z-10"></div>
+                                        <p className="text-white font-extrabold uppercase text-3xl">Menu</p>
+                                </div>
+                        </div>
+                        <div className="menu-overlay">
+                                <div className="menu-overlay-bar flex justify-between w-full items-center">
+                                        <div className="menu-logo">
+                                                <Link to="/" className="logo text-white font-bold text-3xl">
+                                                        Code Dragon
+                                                </Link>
+                                        </div>
+                                        <div
+                                                className="menu-close-icon flex items-center gap-1"
+                                                onClick={toggleMenu}
+                                        >
+                                                <p className="text-white font-bold uppercase hidden md:block">
+                                                        Close
+                                                </p>
+                                                <RiCloseCircleLine size={50} color="white" />
+                                        </div>
+                                </div>
+                                <div className="menu-copy">
+                                        <div className="preview">
+                                                <div className="preview-img preview-img-1"></div>
+                                                <div className="preview-img preview-img-2"></div>
+                                        </div>
+                                        <div className="menu-links flex flex-col items">
+                                                {menuLinks.map((link, index) => (
+                                                        <div
+                                                                className="menu-link-item"
+                                                                key={index}
+                                                                onClick={() => {
+                                                                        navigate(link.path);
+                                                                }}
+                                                        >
+                                                                <div className="menu-link-label">
+                                                                        <p className="text-white">{link.label}</p>
+                                                                </div>
+                                                                <div className="menu-link-info hidden min-[965px]:block">
+                                                                        <Link to={link.path} className="menu-link text-nowrap z-50">
+                                                                                <p className="">{link.info}</p>
+                                                                        </Link>
+                                                                </div>
+                                                                <div className="menu-link-tag">
+                                                                        <p className="">{link.tag}</p>
+                                                                </div>
+                                                        </div>
+                                                ))}
+                                        </div>
+                                </div>
+                        </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="menu-preview"></div>
-      </div>
-    </div>
-  );
+        );
 }
 
 export default HomeMenu;
