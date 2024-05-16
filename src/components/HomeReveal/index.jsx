f729396db860bba98fe77809f5b8ebd47b16881d
Mpinyaz <ebmpinyuri@gmail.com>
2024-05-17 01:18:19 +0200

    Refactor HomeReveal animation initialization. Improve code readability & reusability.

--- src/components/HomeReveal/index.jsx
+++ src/components/HomeReveal/index.jsx
@@ -1,67 +1,10 @@
 import "./HomeReveal.css";
-import { useGSAP } from "@gsap/react";
-import { gsap } from "gsap";
 import { useRef, useEffect, useState } from "react";
-import { animateHomeReveal } from "./animations";
+import { initAnimation } from "./animations";
 import { IoSunnyOutline } from "react-icons/io5";
 export function HomeReveal() {
   const [isActive, setIsActive] = useState(false);
   const homeRevealRef = useRef(null);
-  const tlLeft = gsap.timeline({ defaults: { ease: "expo.inOut" } });
-
-  const tlRight = gsap.timeline();
-  const initAnimation = () => {
-    const panel = document.querySelector(".home_reveal_left_container");
-    const homeRevealLeftContainerTitle = document.querySelectorAll(
-      ".home_reveal_left_container_title > .ofh > h1",
-    );
-    const homeLeftSvg = document.querySelector(
-      ".home_reveal_left_container_rect_image",
-    );
-    const button = document.querySelector(".home_teal");
-    tlLeft
-      .to(panel, { duration: 1.5, y: "100%" }, 0.05)
-      .to(
-        homeRevealLeftContainerTitle,
-        { duration: 0.9, y: "-100%", stagger: 0.025 },
-        0,
-      )
-      .fromTo(
-        homeLeftSvg,
-        { autoAlpha: 0, rotate: 0 },
-        { autoAlpha: 1, rotate: 360, duration: 3 },
-      )
-      .to(
-        [button, ".home_teal_text"],
-        { duration: 1.5, color: "orange", backgroundColor: "black" },
-        0,
-      );
-
-    const homeRevealRightContainerTitle = document.querySelectorAll(
-      ".home_reveal_right_container_title > .ofh > h1",
-    );
-    const homeRevealRightContainerParagraph = document.querySelector(
-      ".home_reveal_right_container_paragraph > p",
-    );
-    gsap.set(
-      [homeRevealRightContainerTitle, homeRevealRightContainerParagraph],
-      {
-        y: "-100%",
-      },
-    );
-    tlRight
-      .to(".reveal_top", {
-        duration: 1.5,
-        y: "-100%",
-        ease: "expo.inOut",
-      })
-      .to(
-        [homeRevealRightContainerTitle, homeRevealRightContainerParagraph],
-        { duration: 3, y: 0, ease: "power4.out", stagger: 0.025 },
-        0.05,
-      );
-    return gsap.timeline({ paused: true }).add(tlLeft, 0).add(tlRight, 0);
-  };
 
   const handleAnimation = () => {
     if (!isActive) {
